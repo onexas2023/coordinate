6e9323e1b4ecfd3e4d53368f4e35607c9fd4839b
Dennis Chen <atticcat@gmail.com>
2023-05-10 17:03:23 +0800

    implement default-roles

---
+++ coordinate-shell/src/main/java/onexas/coordinate/service/domain/Constants.java
@@ -0,0 +1,10 @@
+package onexas.coordinate.service.domain;
+/**
+ * 
+ * @author Dennis Chen
+ *
+ */
+public class Constants {
+
+	public static final String CONFIG_DEFAULT_ROLES = "default-roles";
+}
